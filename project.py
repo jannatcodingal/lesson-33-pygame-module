1db4f923fc75f1d4ab39cdc7f146650da80cef21
jannatcodingal <jannatrehman007@gmail.com>
2025-06-25 15:26:39 +0500

    project

---
+++ project.py
@@ -0,0 +1,13 @@
+import pygame
+pygame.init()
+import pygame
+background_colour = (58, 58, 58)
+screen = pygame.display.set_mode((500, 500))
+pygame.display.set_caption('My First Game Screen')
+screen.fill(background_colour)
+pygame.display.flip()
+running = True
+while running:
+    for event in pygame.event.get(): 
+        if event.type == pygame.QUIT:
+            running = False
